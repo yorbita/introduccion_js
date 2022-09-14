a31430e862134c01505499d003d8c82cbc0719ad
Yorbelys <yorbe2406@gmail.com>
2022-09-14 10:30:07 -0300

    desafio

---
+++ main.js
@@ -0,0 +1,57 @@
+var hola = 'hola mundo'
+var Hola = 'Hola Yorb'
+var $hola = 'Hola billete'
+var _hola = 'hola guion bajo'
+
+var adios= 1,
+Adios='2',
+$adios='3';
+
+console.log(hola)
+console.log(Hola)
+console.log ($adios)
+
+
+/* Crea una lista con los siguientes elementos:
+
+- Tu nombre (string)
+- Tu apellido (string)
+- Tu edad (number)
+- Â¿Eres desarrollador de aplicaciones web? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+- Tu pasatiempos favoritos (Array)
+- Declara una funcion miActividadFavorita la cual retorne un string con la accion que mas te guste realizar */
+
+var nombre = ["Yorbelys"]
+console.log(nombre);
+
+var apellido  = ["Brito"]
+console.log(apellido);
+
+var edad = [27]
+console.log(edad);
+
+var dev = Boolean(true)
+console.log(dev);
+
+var nacimiento = new Date("06-24-1995")
+console.log(nacimiento);
+
+var libro = {
+    nombre:"100 años de soledad", 
+    autor:"Gabriel García Marquez", 
+    fecha:"1967", 
+    url:"https://es.wikipedia.org/wiki/Cien_a%C3%B1os_de_soledad"}
+console.log(libro);
+
+var pasatiemposs = ["leer", "ver anime", "estudiar", "dormir"]
+console.log(pasatiemposs);
+
+actividad = function  () {
+console.log("amo leer");
+};
+
+
+
+
